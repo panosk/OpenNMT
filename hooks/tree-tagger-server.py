c83505662268da873fbf471dd0bec12f92c1e31c
Panos Kanavos <panoskanavos@gmail.com>
2018-03-05 23:23:09 +0200

    Cleanup and code styling

--- hooks/tree-tagger-server.py
+++ hooks/tree-tagger-server.py
@@ -8,40 +8,35 @@ import threading
 import argparse
 import re
 
-
 def start_model(path, m, l, l_first):
   global treetagger
   global nbuf
   global only_lemma
   global lemma_first
-  lemma_first = l_first
 
   only_lemma = False
-
-  tagger_option = ''
   lemma = False
+  lemma_first = l_first
+  tagger_option = ''
   lemma_no_unknown = ''
+
   if l == 'none':
-      tagger_option = None
-  elif l == 'with' or l == 'only':
-      if l == 'only':
-          only_lemma = True
-      tagger_option = '-lemma'
-      lemma_no_unknown = '-no-unknown'
+    tagger_option = None
+  else:
+    if l == 'only':
+      only_lemma = True
+    tagger_option = '-lemma'
+    lemma_no_unknown = '-no-unknown'
+    lemma = True
 
-  if tagger_option is not None:
-      lemma = True
-
-  treetagger = subprocess.Popen([path+'/tree-tagger']+[tagger_option] * lemma + [lemma_no_unknown] * lemma + [m],
-                                stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=open(os.devnull, 'w'),
-                                universal_newlines=True)
+  treetagger = subprocess.Popen([path + '/tree-tagger'] + [tagger_option] * lemma + [lemma_no_unknown] * lemma + [m],
+                                stdout = subprocess.PIPE, stdin = subprocess.PIPE, stderr = open(os.devnull, 'w'),
+                                universal_newlines = True)
 
   # this parameter should be adjusted for each os - it forces tree-tagger to flush by following this many sentence ends
   nbuf = 3000
 
-
 extraneous = 0
-
 
 def tag(s):
   global extraneous
@@ -50,9 +45,9 @@ def tag(s):
   for w in l:
     # if there's a joiner before the word, get rid of the joiner
     if w[0] == joiner_char:
-      treetagger.stdin.write(w[1:]+'\n')
+      treetagger.stdin.write(w[1:] + '\n')
     else:
-      treetagger.stdin.write(w+'\n')
+      treetagger.stdin.write(w + '\n')
   treetagger.stdin.write('\n')
   for _ in range(nbuf):
     treetagger.stdin.write('.\n')
@@ -63,9 +58,9 @@ def tag(s):
     if tag != '':
       if extraneous == 0:
         if only_lemma:
-            tag = tag[tag.index('\t')+1:]
+          tag = tag[tag.index('\t') + 1:]
         if lemma_first:
-            tag = tag[tag.index('\t')+1:]+'\t'+tag[0:tag.index('\t')]
+          tag = tag[tag.index('\t') + 1:] + '\t' + tag[0:tag.index('\t')]
         result.append(tag)
       else:
         extraneous -= 1
@@ -119,14 +114,14 @@ class SimpleHttpServer():
     self.waitForThread()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='TreeBank Wrapper')
   parser.add_argument('-sent', type=str, help='Test commandline mode - pass the sentence to tag')
   parser.add_argument('-port', default=3000, type=int, help='Listening port for HTTP Server')
   parser.add_argument('-ip', default="localhost", help='HTTP Server IP')
   parser.add_argument('-model', type=str, help='model to serve')
   parser.add_argument('-path', type=str, help='path to tree-tagger binaries')
-  parser.add_argument('-lemma', default="none", choices=['none', 'with',' only'], help='Tag with lemmas. \
+  parser.add_argument('-lemma', default="none", choices=['none', 'with', 'only'], help='Tag with lemmas. \
           \'with\' appends the lemma after the tag, \'only\' appends only the lemma')
   parser.add_argument('-lemma_first', action='store_true', help='when tagging both with pos and lemmas, append the lemma first')
   args = parser.parse_args()
